009f3cb853c2e78a5bd0ed8408c08d9ac8ffe726
tim.jerman <tim.jerman@fe.uni-lj.si>
2017-05-29 15:21:51 +0200

    added preprocessing to 2D example

--- example_vesselness2D.m
+++ example_vesselness2D.m
@@ -3,9 +3,16 @@
 % load input image
 I = imread('fundus2D.png');
 
+% preprocess the input a little bit
+Ip = single(I);
+thr = prctile(Ip(Ip(:)>0),1) * 0.9;
+Ip(Ip<=thr) = thr;
+Ip = Ip - min(Ip(:));
+Ip = Ip ./ max(Ip(:));    
+
 % compute enhancement for two different tau values
-V1 = vesselness2D(I, 0.5:0.5:2.5, [1;1], 1, false);
-V2 = vesselness2D(I, 0.5:0.5:2.5, [1;1], 0.5, false);
+V1 = vesselness2D(Ip, 0.5:0.5:2.5, [1;1], 1, false);
+V2 = vesselness2D(Ip, 0.5:0.5:2.5, [1;1], 0.5, false);
 
 % display result
 figure; 
